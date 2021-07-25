24f763347b20bd7a2dabcd0e4ad86c341259ef1c
Bruno Henrique Santos <brunohqsantos@hotmail.com>
2021-07-24 22:07:54 -0300

    Upload User Avatar Service

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
 import UsersController from '../controllers/UsersController';
 import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 
 const usersController = new UsersController();
+const usersAvtarConstroller = new UserAvatarController();
+
+const upload = multer(uploadConfig);
 
 usersRouter.get('/', isAuthenticated, usersController.index);
 
@@ -21,5 +27,11 @@ usersRouter.post(
   usersController.create
 );
 
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  usersAvtarConstroller.update,
+);
 
 export default usersRouter;
